9b3b99d33e6044f6e17bf704b567703b00618e2f
Maryam Khodabakhshloo <49555798+MaryamKhodabakhshloo@users.noreply.github.com>
2020-01-21 16:51:02 -0500

    Add files via upload

--- game.py
+++ game.py
@@ -1,11 +1,11 @@
 import os
 import pygame
+import numpy as np
 from math import sin, radians, degrees, copysign
 from pygame.math import Vector2
 
-##########################################  CODE   ##################################################
-
-class Car:
+#################################################################################
+class EGO_Car:
     def __init__(self, x, y, angle=90.0, length=4, max_steering=30, max_acceleration=5.0):
         self.position = Vector2(x, y)
         self.velocity = Vector2(0.0, 0.0)
@@ -30,34 +30,52 @@ class Car:
             angular_velocity = 0
 
         self.position += self.velocity.rotate(-self.angle) * dt
+        print(self.position)
         self.angle += degrees(angular_velocity) * dt
+################################################################################
 
+class O_Car:
+    def __init__(self, x, y, length=4):
+        self.position = Vector2(x, y)
+        self.velocity = Vector2(0.0, 20.0)
+        self.length = length
+        self.max_velocity = 100
 
+    def update(self,dt):
+        self.velocity=(0,-5)
+        self.position += np.array(self.velocity) * dt
+
+################################################################################
 class Game:
     def __init__(self):
         pygame.init()
-        #####pygame.display.set_caption("Car tutorial")
+                   #####pygame.display.set_caption("Car tutorial")
         width = 650
-        height = 713
-        BLUE =(53, 115, 255)
+        height = 970
         self.screen = pygame.display.set_mode((width, height))
-        
         self.clock = pygame.time.Clock()
         self.ticks = 60
         self.exit = False
 
     def run(self):
         current_dir = os.path.dirname(os.path.abspath(__file__))
-        image_path = os.path.join(current_dir, "car.png")
-        car_image = pygame.image.load(image_path)
-
+        path = os.path.join(current_dir, "car.png")
+        path1 = os.path.join(current_dir, "car1.png")
+        path2 = os.path.join(current_dir, "car2.png")
+        path3 = os.path.join(current_dir, "car3.png")
+        car_image = pygame.image.load(path)
+        car_image1 = pygame.image.load(path1) 
+        car_image2 = pygame.image.load(path2)
+        car_image3 = pygame.image.load(path3)
+        car = EGO_Car(14,19,angle=90.0)
+        car1=O_Car(6,5)
+        car2=O_Car(6,12)
+        car3=O_Car(6,19)
+        ppu = 32
         
-        car = Car(30,31)
-        ppu = 20
-
         while not self.exit:
             dt = self.clock.get_time() / 1000
-
+            
             # Event queue
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -94,18 +112,32 @@ class Game:
 
             # Logic
             car.update(dt)
-
+            car1.update(dt)
+            car2.update(dt)
+            car3.update(dt)
             # Drawing
             self.screen.fill((150, 150, 150))
-            rotated = pygame.transform.rotate(car_image, car.angle)
+            pygame.draw.rect(self.screen,(255, 255, 255) , [50, 0, 13, 970])
+            pygame.draw.rect(self.screen,(255, 255, 255) , [585, 0, 13, 970])
+            pygame.draw.rect(self.screen,(255, 255, 255) , [320, 0, 13, 970])
+            rotated  = pygame.transform.rotate(car_image, car.angle)
+            rotated1 = car_image1 
+            rotated2 = car_image2
+            rotated3 = car_image3
             rect = rotated.get_rect()
+            rect1 = rotated1.get_rect()
+            rect2 = rotated1.get_rect()
+            rect3 = rotated1.get_rect()
             self.screen.blit(rotated, car.position * ppu - (rect.width / 2, rect.height / 2))
+            self.screen.blit(rotated1, car1.position * ppu - (rect1.width / 2, rect1.height / 2))
+            self.screen.blit(rotated2, car2.position * ppu - (rect2.width / 2, rect2.height / 2))
+            self.screen.blit(rotated3, car3.position * ppu - (rect3.width / 2, rect3.height / 2))
             pygame.display.flip()
 
             self.clock.tick(self.ticks)
         pygame.quit()
 
-
+################################################################################
 if __name__ == '__main__':
     game = Game()
     game.run()
